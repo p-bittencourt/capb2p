f886b4d0ab50d7aa69b4faabf4efa4567032faf5
pb-sap <pedro.bittencourt@spro.com.br>
2025-09-18 19:55:16 +0000

    end of lesson 13

--- srv/main.js
+++ srv/main.js
@@ -5,6 +5,12 @@ class Main extends cds.ApplicationService {
     init() {
         const { Books } = this.entities;
 
+        // Don't forget to pass on "return next()" on an on handler
+        this.on('READ', Books, (_, req) => {
+            // do something
+            return next();
+        })
+
         this.after('READ', Books, books => {
             books.map(b => console.log(b.title))
         })
