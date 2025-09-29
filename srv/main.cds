fb29ccb6e0aec8e03c51fe3dd785a09cadd1e631
pb-sap <pedro.bittencourt@spro.com.br>
2025-09-29 19:56:33 +0000

    end of lesson 15

--- srv/main.cds
+++ srv/main.cds
@@ -1,5 +1,8 @@
 using org from '../db/schema';
 
+extend org.pbsap.Books with {
+    virtual urgency: String;
+}
 service bookshop {
     entity Books as projection on org.pbsap.Books;
     entity Authors as projection on org.pbsap.Authors;
--- srv/main.js
+++ srv/main.js
@@ -6,6 +6,17 @@ class Main extends cds.ApplicationService {
     init() {
         const { Books } = this.entities;
 
+        const changeUrgencyDueToSubject = (data) => {
+            if (data) {
+                const books = Array.isArray(data) ? data : [data];
+                books.forEach((book) => {
+                    if (book.title?.toLowerCase().includes('startup')) {
+                        book.urgency = 'HIGH'
+                    }
+                }) 
+            }
+        }
+
         // Don't forget to pass on "return next()" on an on handler otherwise content will not be returned
         this.on('READ', Books, (req, next) => {
             // do something
@@ -13,17 +24,20 @@ class Main extends cds.ApplicationService {
         })
 
         // After handlers do not require the return statement to work properly
-        // 
-        this.after('READ', Books, books => {
-            if (books) {
-                const booksArray = Array.isArray(books) ? books : [books]; 
-                booksArray.map(b => logger(b.title))
-            }
-        })
+        // this.after('READ', Books, books => {
+        //     if (books) {
+        //         const booksArray = Array.isArray(books) ? books : [books]; 
+        //         booksArray.map(b => logger(b.title))
+        //     }
+        // })
 
-        // This is vital for correct framework functionality
+        this.after('READ', Books, changeUrgencyDueToSubject);
+
+        // This is vital for correct functionality of the generic handlers
         return super.init();
     }
+
+    
 }
 
 module.exports = Main;
